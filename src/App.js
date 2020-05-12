e77a4b1464b8f26f621d08d955f2347bc35de591
FurkaanHossain <efurkaan@rams.colostate.edu>
2020-05-12 23:39:47 +0600

    client side JSON validation, schemas now in .js files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
-import {SERVER_CONFIG_REQUEST} from "./Constants";
+import {SERVER_CONFIG_REQUEST, HTTP_BAD_REQUEST} from "./Constants";
+import {isJsonResponseValid} from "./utils/restfulAPI";
+import {createErrorBanner} from "./CheckErrorStatus";
 import axios from 'axios';
+import {configSchema} from "./schemas/ConfigResponse";
 import Header from "./Components/Margins/Header";
 import Footer from "./Components/Margins/Footer";
 
@@ -9,31 +12,42 @@ export default class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            config : null
+            config : null,
+            errorMessage : null
         }
-    }
-
-    getConfig(){
-        //initally CORS violation--> fix is in server//Can create a REST class with all http reqs
-        //this should also validate response, hence changin the name to "processGetResponse"
-        axios.get(SERVER_CONFIG_REQUEST).then(response => {
-            console.log(response.data);
-            this.setState({config: response});
-        });
-    }
-
-
-    render() {
         if(this.state.config === null){
             this.getConfig();
         }
+    }
+
+    render() {
         return (//NEED TO PUT EVERYTHING IN PAGE CONTAINER, THEN EVERYTHING BUT FOOTER IN CONTENT-WRAP FOR FOOTER TO STAY DOWN
             <div className="page-container">
                 <div className="content-wrap">
                     <Header/>
+                    {this.state.errorMessage}
                 </div>
                 <Footer config = {this.state.config}/>
             </div>
         );
     }
+
+    getConfig(){
+        axios.get(SERVER_CONFIG_REQUEST).then(response => {
+            this.processConfigResponse(response)
+        });
+    }
+
+    processConfigResponse(configResponse){
+        if(!isJsonResponseValid(configResponse.data, configSchema)) {
+            this.processServerConfigError("INVALID_RESPONSE", HTTP_BAD_REQUEST, `Configuration response not valid`);
+        }else{
+            this.setState({config: configResponse})
+        }
+    }
+
+    processServerConfigError(statusText, statusCode, message) {
+        this.setState({config: null, errorMessage: createErrorBanner(statusText, statusCode, message)});
+    }
+
 }
